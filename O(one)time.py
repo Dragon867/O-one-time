59d1861a46388f753d089cfbe79b41e816587cf7
Dragon867 <hamza.jadoon2007@gmail.com>
2024-11-18 07:21:12 -0800

    Add files via upload

---
+++ O(one)time.py
@@ -0,0 +1,8 @@
+def prime_number(n):
+    iteration=0
+    print("The number enter by the user is : ",n)
+    iteration+=1
+    print("Totla iterations done by the code is : ",iteration)
+prime_number(74)
+prime_number(695)
+print("With any n the time taken by the code wont change")
